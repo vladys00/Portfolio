1a96ca5fc37410799e0ae1a7be339d223da09fcc
Vladys K <vldk0022@gmail.com>
2025-03-29 19:13:34 +0100

    created a basic structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
