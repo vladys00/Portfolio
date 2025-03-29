ef7a5ab0d158eee17d337516a80278ac53f25335
Vladys K <vldk0022@gmail.com>
2025-03-30 00:31:53 +0100

    tailwind setting working correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  // ...
+  integrations: [tailwind()],
 });
