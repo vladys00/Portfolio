183bddf6a342a69ba747a3a5005692473cb6e365
Vladys K <vldk0022@gmail.com>
2025-04-02 13:00:18 +0200

    finished main styles for the projects page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // ...
   integrations: [tailwind()],
+  experimental: {
+      svg: true,
+    },
+   
 });
