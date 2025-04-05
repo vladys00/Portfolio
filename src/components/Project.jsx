cd6bdf7e23cf7f72e17abccb976212ddbc0e1a43
Vladys K <vldk0022@gmail.com>
2025-04-05 17:58:33 +0200

    styled index with words

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,17 +13,20 @@ function Project({
 }) {
   const [dropDown, useDropDown] = useState(false);
   return (
-    <div className=" w-[340px] sm:w-auto lg:w-[400px] ">
+    <div className=" w-[340px]  sm:w-auto lg:w-[400px] flex flex-col ">
       <div className="rounded-lg overflow-hidden mt-4 lg:w-[400px]">
         <img  className="w-[400pxs]" src={frontImage} alt="image of project" />
       </div>
-      <div className="p-2 ">
+      <div className="p-2  ">
         <h2 className="font-bold">{name}</h2>
-        <p className="mt-2">{shortDescription}</p>
+        <p className="pe-1 mt-1 w-[350px]">{shortDescription}</p>
       </div>
-      <div className="flex flex-wrap">
+      
+      <div className="flex flex-wrap mt-[auto]">
         {tags.map((tag, index)=><div className={`px-4 py-1 me-2 mt-2 ${index === 0 ? "border border-black" :"border border-black rounded-[24px]" } `}>{tag}</div>)}
       </div>
+
+      
     </div>
   );
 }
