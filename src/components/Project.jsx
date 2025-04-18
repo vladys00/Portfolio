fe0ce8e9a08727697b69ee519ac5969fbbd4eb85
Vladys K <vldk0022@gmail.com>
2025-04-18 18:14:42 +0200

    final portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -16,13 +16,13 @@ function Project({
 }) {
   const [dropDown, useDropDown] = useState(false);
   return (
-    <div className=" w-[340px]  sm:w-auto lg:w-[400px] flex flex-col ">
+    <div className=" w-[340px]  sm:w-auto lg:w-[400px] flex flex-col mb-3 ">
       <a href={link} className="rounded-lg overflow-hidden mt-4 lg:w-[400px] relative w-full h-full group">
         <img  className="w-[400pxs] w-full h-full object-cover" src={frontImage} alt="image of project" />
         <div class="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
       </a>
       <div className="p-2  ">
-        <h2 className="font-bold">{name}</h2>
+        <h2 className="font-bold hover:text-blue-400"><a href={liveDemo}>{name}</a></h2>
         <p className="pe-1 mt-1 w-[350px]">{shortDescription}</p>
       </div>
       
